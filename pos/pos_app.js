fdcb4cc385589c934c7d7435800ddd01cc5b986a
dduran19 <denverjames.duran19@outlook.com>
2023-06-14 02:34:27 +0800

    Converted flavorList from ul to div

--- pos/pos_app.js
+++ pos/pos_app.js
@@ -11,14 +11,36 @@ fetch("milkteaStock.json")
 
 function generateFlavor(data){
     data.forEach(item => {
-        const newItem = document.createElement("li");
-        const newItemText = `${item.flavor} - ${item.instock ? "In Stock" : "Out of Stock"} (${item.amount})`;
-        const newItemClass = item.instock ? "flavor-item" : "flavor-item red" 
+        const newItem = document.createElement("div")
+
+        const newItemName = document.createElement("div");
+        const newItemInstock = document.createElement("div");
+        const newItemAmount = document.createElement("div");        
+        newItemName.appendChild(document.createTextNode(item.flavor));
+
+        const checkmark = document.createElement("span");
+        checkmark.innerHTML = "&#x2713; ";
         
+        let newItemInstockText;
+        if (item.instock){
+            checkmark.setAttribute("class","checkmark green");
+            newItemInstockText = "In Stock";
+        } else {
+            checkmark.setAttribute("class","checkmark red");
+            newItemInstockText = "Out of Stock";}
+        
+        newItemInstock.appendChild(checkmark);
+        newItemInstock.appendChild(document.createTextNode( newItemInstockText));
+
+        const newItemClass = item.instock ? "flavor-item" : "flavor-item red" ;
+        newItemInstock.setAttribute("class", newItemClass)
+        newItemAmount.appendChild(document.createTextNode(item.amount))
+
         newItem.setAttribute("id",item.flavor);
-        newItem.setAttribute("class", newItemClass);
-        newItem.appendChild(document.createTextNode(newItemText))
-        
+        newItem.setAttribute("class", "flavor-container");
+        newItem.appendChild(newItemName)
+        newItem.appendChild(newItemInstock)
+        newItem.appendChild(newItemAmount)
         flavorList.appendChild(newItem)
     });
 }
