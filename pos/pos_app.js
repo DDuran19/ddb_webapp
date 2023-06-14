b4f4fb4cb28415c3b4717519c279bbe3fd5cd3b0
Denver James Duran <denverjames.duran19@outlook.com>
2023-06-15 05:14:39 +0800

    Add function for modifying the specifics of order

--- pos/pos_app.js
+++ pos/pos_app.js
@@ -1,3 +1,12 @@
+const CLASS = "class";
+const ID = "id";
+const SPAN = "span";
+const DIV = "div";
+const INPUT = "input";
+const RADIO = "radio";
+const CHECKBOX = "checkbox";
+const LABEL = "label"
+
 const flavorList = document.getElementById("flavorList");
 fetch("milkteaStock.json")
     .then((response) =>{
@@ -12,39 +21,39 @@ fetch("milkteaStock.json")
 function generateFlavor(data){
     data.sort((min, max) => max.amount - min.amount);
     data.forEach(item => {
-        const newItemRadio = document.createElement("input")
-        newItemRadio.setAttribute("id",item.flavor);
+        const newItemRadio = document.createElement(INPUT)
+        newItemRadio.setAttribute(ID,item.flavor);
         newItemRadio.setAttribute("name", "flavors");
-        newItemRadio.setAttribute("type","radio");
+        newItemRadio.setAttribute("type",RADIO);
         newItemRadio.setAttribute("value", item.flavor);
 
         flavorList.appendChild(newItemRadio)
-        const newItem = document.createElement("label");
-        const newItemName = document.createElement("span")
-        const checkmark = document.createElement("span");
-        const newItemInstock = document.createElement("span")
+        const newItem = document.createElement(LABEL);
+        const newItemName = document.createElement(SPAN)
+        const checkmark = document.createElement(SPAN);
+        const newItemInstock = document.createElement(SPAN)
         const newItemClass = item.instock ? "flavor-item green" : "flavor-item red" ;
 
-        newItem.setAttribute("class", "container flavor-container");
+        newItem.setAttribute(CLASS, "container flavor-container");
         newItem.setAttribute("for",item.flavor);
         newItemName.appendChild(document.createTextNode(item.flavor))     
 
         let newItemInstockText;
         let newItemAmount;
         if (item.instock){
-            checkmark.setAttribute("class","checkmark green");
+            checkmark.setAttribute(CLASS,"checkmark green");
             newItemInstockText = "In Stock";
             checkmark.innerHTML = "&#x2713; ";
-            newItemName.setAttribute("class", "flavor-name");
-            newItemAmount = document.createElement("span");
+            newItemName.setAttribute(CLASS, "flavor-name");
+            newItemAmount = document.createElement(SPAN);
             newItemAmount.appendChild(document.createTextNode(item.amount))            
         } else {
-            checkmark.setAttribute("class","checkmark red");
-            newItemName.setAttribute("class", "flavor-name red");
+            checkmark.setAttribute(CLASS,"checkmark red");
+            newItemName.setAttribute(CLASS, "flavor-name red");
             newItemInstockText = "Out of Stock";
             checkmark.innerHTML = "&#x2717; ";}
 
-        newItemInstock.setAttribute("class", newItemClass)
+        newItemInstock.setAttribute(CLASS, newItemClass)
         newItemInstock.appendChild(document.createTextNode(newItemInstockText));
         newItem.appendChild(newItemName);
         newItem.appendChild(document.createElement("br"));
@@ -59,6 +68,7 @@ function generateFlavor(data){
 };
 
 const seriesContainer = document.getElementById("series-container")
+const addonContainer = document.getElementById("addon-container")
 fetch("milktea.json")
     .then((response) => {
         if (!response.ok){
@@ -68,11 +78,27 @@ fetch("milktea.json")
         }
     })
     .then((response) => {
-        generateAddons(response)
+        generateMilkteaComponents(response,"series", RADIO, seriesContainer);
+        generateMilkteaComponents(response,"addons", RADIO, addonContainer);
+
     });
 
-function generateAddons(data){
-    data.forEach(addOn => {
+function generateMilkteaComponents(response, Type, htmlElement,htmlContainer){
+    let i = 0;
+    response[Type].forEach((item) => {
+        const container = document.createElement(DIV);
+        container.setAttribute(CLASS, "container")
+        const itemNameRadio = document.createElement(INPUT);
+        const itemName = document.createElement(LABEL)
+        itemName.appendChild(document.createTextNode(item))
+        itemNameRadio.setAttribute(ID,item);
+        htmlElement === RADIO ? itemNameRadio.setAttribute("name", Type): false;
+        itemNameRadio.setAttribute("type",htmlElement);
+        itemNameRadio.setAttribute("value", item);
         
+        container.appendChild(itemNameRadio);
+        container.appendChild(itemName)
+        htmlContainer.appendChild(container);
+        i++
     });    
 }
