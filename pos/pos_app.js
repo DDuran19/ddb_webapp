96b9445513d5c1ccf21ae6ad16b7b338667dd9b2
Denver James Duran <denverjames.duran19@outlook.com>
2023-06-14 08:03:48 +0800

    some progress

--- pos/pos_app.js
+++ pos/pos_app.js
@@ -10,31 +10,38 @@ fetch("milkteaStock.json")
     .then((response) => {generateFlavor(response)});
 
 function generateFlavor(data){
+    data.sort((min, max) => max.amount - min.amount);
     data.forEach(item => {
         const newItem = document.createElement("div")
 
         const newItemName = document.createElement("div");
-        const newItemInstock = document.createElement("div");
-        const newItemAmount = document.createElement("div");        
         newItemName.appendChild(document.createTextNode(item.flavor));
 
+        const newItemInstock = document.createElement("div");
+        const newItemAmount = document.createElement("div");        
         const checkmark = document.createElement("span");
-        checkmark.innerHTML = "&#x2713; ";
+        
+        
         
         let newItemInstockText;
         if (item.instock){
             checkmark.setAttribute("class","checkmark green");
             newItemInstockText = "In Stock";
+            checkmark.innerHTML = "&#x2713; ";
+            newItemName.setAttribute("class", "flavor-name");
+            newItemAmount.appendChild(document.createTextNode(item.amount));
         } else {
             checkmark.setAttribute("class","checkmark red");
-            newItemInstockText = "Out of Stock";}
+            newItemName.setAttribute("class", "flavor-name red");
+            newItemInstockText = "Out of Stock";
+            checkmark.innerHTML = "&#x2717; ";}
         
         newItemInstock.appendChild(checkmark);
         newItemInstock.appendChild(document.createTextNode( newItemInstockText));
 
-        const newItemClass = item.instock ? "flavor-item" : "flavor-item red" ;
+        const newItemClass = item.instock ? "flavor-item green" : "flavor-item red" ;
         newItemInstock.setAttribute("class", newItemClass)
-        newItemAmount.appendChild(document.createTextNode(item.amount))
+        
 
         newItem.setAttribute("id",item.flavor);
         newItem.setAttribute("class", "flavor-container");
@@ -43,4 +50,22 @@ function generateFlavor(data){
         newItem.appendChild(newItemAmount)
         flavorList.appendChild(newItem)
     });
+};
+
+const seriesContainer = document.getElementById("series-container")
+fetch("milktea.json")
+    .then((response) => {
+        if (!response.ok){
+            return "No Series to show"
+        } else {
+            return response.json();
+        }
+    })
+    .then((response) => {
+        generateAddons(response)
+    })
+function generateAddons(data){
+    data.forEach(addOn => {
+        
+    });    
 }
